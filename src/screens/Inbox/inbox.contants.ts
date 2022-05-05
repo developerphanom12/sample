a69a2974982f9471d3f8bf1e1299bb1510d8085f
Vadim Diachenko <75821783+diachenkovadim@users.noreply.github.com>
2022-05-05 16:02:49 +0300

    feature/dashboard-empty-screen (#43)
    Co-authored-by: Vadim <vadim.dyachenko.2018@ukr.net>

--- src/screens/Inbox/inbox.contants.ts
+++
@@ -1,7 +0,0 @@
-export const EMPTY_DATA_STRINGS = {
-  title: 'Uploaded receipts will be shown here...',
-  firstSubtitle:
-    'We have created everything for you, please upload your receipts to start.',
-  secondSubtitle: 'You can do that by using Add button below',
-  buttonText: 'Add',
-};
