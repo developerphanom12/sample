e7522405f2f862af527b296c0504fe91cc10a46a
Vadim <vadim.dyachenko.2018@ukr.net>
2022-06-06 14:29:38 +0300

    feature/login-media-requests

--- src/screens/Login/Login.style.ts
+++ src/screens/Login/Login.style.ts
@@ -21,11 +21,6 @@ export const Styled = {
     width: 100%;
     height: 100%;
   `,
-  Wrapper: styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-  `,
   RightSideContentWrapper: styled.div`
     display: flex;
     flex-direction: column;
