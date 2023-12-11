ec1e1d7840c453ca43eade8787eb5aa5087f386c
Yanovski Egor <egor@MacBook-Pro-Yanovski.local>
2023-12-11 19:23:42 +0200

    add https backend

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ const ENV = process.env.REACT_APP_ENV || 'local';
 
 const ENV_HASH_MAP: Record<string, string> = {
   local: 'http://localhost:3000/',
-  development: 'http://3.9.95.221/',
+  development: 'https://back.receipt-hub.co.uk/',
   staging: 'http://35.176.97.72/',
 };
 
